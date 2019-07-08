9ec7919fccde9ebcfa4061c067c7eecaeb4b4a23
Marissa Ramirez <mzweig@berkeley.edu>
2019-07-08 14:36:03 -0700

    Initial empty source files.

---
+++ src/Geometry.h
@@ -0,0 +1,9 @@
+#ifndef _GEOMETRY_H
+#define _GEOMETRY_H
+
+class Geometry{
+  public:
+    Geometry();
+};
+
+#endif
---
+++ src/makeGeometry.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "Geometry.h"
+
+using namespace std;
+
+int main(){
+  cout << "test" << endl;
+  return 0;
+}
