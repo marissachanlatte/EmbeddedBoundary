4ee0a4d244f5f39dc3f99e533b7fe700b25c9e81
Marissa Ramirez <mzweig@berkeley.edu>
2019-08-06 10:40:46 -0700

    Quad Tree class.

--- src/helpers/geometry_objects.cc
+++ src/helpers/geometry_objects.cc
@@ -1,5 +1,7 @@
 # include "helpers/geometry_objects.h"
 
+#include <vector>
+
 namespace boundary {
 
 namespace helpers {
@@ -8,16 +10,73 @@ Point::Point(){
   x_val = y_val = 0;
 };
 
+
 Point::Point(double first_dim, double second_dim){
   x_val = first_dim;
   y_val = second_dim;
 };
+
 
 Point Point::operator + (const Point &a_point){
   Point result;
   result.x_val = x_val + a_point.x_val;
   result.y_val = y_val + a_point.y_val;
   return result;
+};
+
+
+QuadTree::QuadTree(QuadTree* north_west,
+                   QuadTree* north_east,
+                   QuadTree* south_west,
+                   QuadTree* south_east){
+  north_west_ = north_west;
+  north_east_ = north_east;
+  south_west_ = south_west;
+  south_east_ = south_east;
+};
+
+
+QuadTree::QuadTree(Point* cell_center, int degree){
+  cell_center_ = cell_center;
+  degree_ = degree;
+  normals_.resize(degree);
+  volume_moments_.resize(degree);
+  boundary_moments_.resize(degree);
+};
+
+
+void QuadTree::AssignNormals(double value, int degree){
+  normals_[degree] = value;
+};
+
+
+void QuadTree::AssignVolume(double value, int degree){
+  volume_moments_[degree] = value;
+};
+
+
+void QuadTree::AssignBoundary(double value, int degree){
+  boundary_moments_[degree] = value;
+};
+
+
+Point* QuadTree::GetCellCenter(){
+  return cell_center_;
+};
+
+
+double QuadTree::GetNormal(int degree){
+  return normals_[degree];
+};
+
+
+double QuadTree::GetVolume(int degree){
+  return volume_moments_[degree];
+};
+
+
+double QuadTree::GetBoundary(int degree){
+  return boundary_moments_[degree];
 };
 
 } // namespace helpers
--- src/helpers/geometry_objects.h
+++ src/helpers/geometry_objects.h
@@ -1,6 +1,8 @@
 #ifndef EMBEDDED_BOUNDARY_HELPERS_GEOMETRY_OBJECTS_H
 #define EMBEDDED_BOUNDARY_HELPERS_GEOMETRY_OBJECTS_H
 
+#include <vector>
+
 namespace boundary {
 
 namespace helpers {
@@ -13,6 +15,42 @@ namespace helpers {
       Point operator + (const Point &a_point);
       double x_val;
       double y_val;
+  };
+
+  class QuadTree{
+    public:
+      QuadTree();
+      QuadTree(QuadTree* north_west,
+               QuadTree* north_east,
+               QuadTree* south_west,
+               QuadTree* south_east);
+      QuadTree(Point* cell_center, int degree);
+      void AssignNormals(double value, int degree);
+      void AssignVolume(double value, int degree);
+      void AssignBoundary(double value, int degree);
+
+      Point* GetCellCenter();
+      double GetNormal(int degree);
+      double GetVolume(int degree);
+      double GetBoundary(int degree);
+    private:
+      // Children
+      QuadTree* north_west_;
+      QuadTree* north_east_;
+      QuadTree* south_west_;
+      QuadTree* south_east_;
+
+      // Cell Center
+      Point* cell_center_;
+      int degree_;
+
+      // Normals
+      std::vector<double> normals_;
+
+      // Moments
+      std::vector<double> volume_moments_;
+      std::vector<double> boundary_moments_;
+
   };
 } // namespace helpers
 
