80f58a77c6796a6db121fe647ab298a4c400e8e5
Marissa Ramirez <mzweig@berkeley.edu>
2019-10-07 12:17:56 -0700

    Outputting volume fracs.

--- src/main.cc
+++ src/main.cc
@@ -7,7 +7,18 @@
 #include <vector>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 
 int main(){
+  boundary::inputs::LineGeometry input;
+  boundary::geometry::Boundary line_boundary = boundary::geometry::Boundary(&input);
+  std::map<std::array<double, 2>, boundary::geometry::geo_info> boundary_cells = line_boundary.BoundaryCells();
+  std::ofstream output;
+  output.open ("../outputs/output_boundary.txt");
+  for (std::map<std::array<double, 2>, boundary::geometry::geo_info>::iterator it=boundary_cells.begin();
+       it != boundary_cells.end(); it++){
+         output << it->first[0] << " " << it->first[1] << " " << it->second.volume_moments[0][0] << std::endl;
+       }
+  output.close();
   return 0;
 }
