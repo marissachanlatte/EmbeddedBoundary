eea202df1bdb7922a153fb1114f6de16a9057f9d
Marissa Ramirez <mzweig@berkeley.edu>
2020-10-29 13:22:42 -0700

    Laplacian tests.

---
+++ src/solvers/tests/solver_tests.cc
@@ -0,0 +1,10 @@
+#include "solvers/laplacian.h"
+#include "inputs/circle/circle_test.h"
+
+#include "gtest/gtest.h"
+
+class SolverTest : public ::testing::Test{
+  protected:
+    SolverTest();
+};
+
